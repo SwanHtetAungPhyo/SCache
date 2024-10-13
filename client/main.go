21036fa412621cb3a8a5b4a61c4ebf1be0ed2629
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2024-10-13 17:10:24 +0200

    Verson1.10

--- client/main.go
+++ client/main.go
@@ -11,40 +11,77 @@ import (
 	"github.com/SwanHtetAungPhyo/Scache/server"
 	"github.com/SwanHtetAungPhyo/Scache/utils"
 )
-func main(){
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/SwanHtetAungPhyo/Scache/constants"
+	"github.com/SwanHtetAungPhyo/Scache/server"
+	"github.com/SwanHtetAungPhyo/Scache/utils"
+)
+
+func main() {
+	// Configure cache server
 	cacheConfig, err := server.NewCofig(
 		server.WithPort(":9000"),
 		server.WithCapacity(200),
 		server.WithExpiration(10 * time.Minute),
 	)
 	if err != nil {
-		panic("Cache Server Configuration Failed")
+		panic("Cache Server Configuration Failed: " + err.Error())
 	}
-	_, err = server.NewScacheServer(cacheConfig)
-	if err != nil{
-		 utils.LogMessage(constants.ERROR, err.Error())
-	}	
 
-		
-	connection,err := net.Dial("tcp","localhost:9000" )
-	if err != nil{
-		return 
+	_, err = server.NewScacheServer(cacheConfig)
+	if err != nil {
+		utils.LogMessage(constants.ERROR, err.Error())
+		return
 	}
-	
+
+	// Connect to the cache server
+	connection, err := net.Dial("tcp", "localhost:9000")
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
 	defer connection.Close()
+
+	// Prepare request object
 	requestObj := map[string]interface{}{
 		"command":    "SET",
 		"key":       "user123",
 		"value":     "John Doe",
-		"expiration": 1697270400,
+		"expiration": time.Now().Add(10 * time.Minute).UnixNano(), // Use current time for expiration
 	}
 
-	jsonrquest, _ := json.Marshal(requestObj)
-	connection.Write(jsonrquest)
-	connection.Write([]byte("\n"))
+	// Marshal request to JSON
+	jsonRequest, err := json.Marshal(requestObj)
+	if err != nil {
+		fmt.Println("Error marshaling request:", err)
+		return
+	}
 
-	message, _ := bufio.NewReader(connection).ReadString('\n')
-	fmt.Print(message)
+	// Send request to server
+	_, err = connection.Write(append(jsonRequest, '\n'))
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
 
-	select{}
+	// Read response from server
+	message, err := bufio.NewReader(connection).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	// Print the server response
+	fmt.Print("Server response:", message)
+
+	// Keep the connection alive (optional)
+	select {}
 }
