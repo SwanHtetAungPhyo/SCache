13ba9c1b2d7ac2b4977a1dc958b46a138cb68e9d
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2024-10-13 06:00:11 +0200

    KKKK

--- Tester/main.go
+++ Tester/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"github.com/SwanHtetAungPhyo/Scache/model"
 	"github.com/SwanHtetAungPhyo/Scache/server"
-	
 )
 
 func main() {
-	var cache .Scache
-	tcpServer, err := server.NewTCPServer(":8000", &cache)
+	var cache model.Scache
+	tcpServer, err :=server.NewTCPServer(":8000", &cache)
 	if err != nil {
 		return
 	}
